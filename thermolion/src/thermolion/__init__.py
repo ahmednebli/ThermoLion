93edb6a5e65ee9d9368cd01edd3a4429ea5cfd2b
ahmed nebli <47035144+ahmednebli@users.noreply.github.com>
2025-12-02 10:40:59 +0100

    Delete thermolion directory

--- thermolion/src/thermolion/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Top-level package for ThermoLion.
-
-This package exposes the :class:`ThermoLion` optimizer.
-"""
-
-from .version import __version__
-from .optimizers import ThermoLion
-
-__all__ = ["ThermoLion", "__version__"]
